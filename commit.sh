889f88b8e95d47582a1977f4ac9a1afcc16fac52
TueNhanLC <tuenhanlc@gmail.com>
2026-05-13 11:37:54 +0700

    at 2026-05-13 11:37:54 with content update file commit

--- commit.sh
+++ commit.sh
@@ -1,5 +1,8 @@
 git status
 git add . 
 read -p "type commit content: " content
+read -p "do you want to clear screen after push? enter y/n: " clear_a
 git commit -m "at $(date +'%Y-%m-%d %H:%M:%S') with content $content"
 git push
+if [[ $clear_a == "y" || $clear_a == "Y" ]]; then clear; fi
+
