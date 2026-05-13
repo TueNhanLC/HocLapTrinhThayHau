b2be5a8f2aa25bfebd100685c8b65b46d23066c7
TueNhanLC <tuenhanlc@gmail.com>
2026-05-13 12:01:34 +0700

    at 2026-05-13 12:01:34 with content update script commit shell

--- commit.sh
+++ commit.sh
@@ -1,8 +1,13 @@
+
+git status
+
+if [[ -z $(git status --porcelain) ]]; then
+    echo -e "\n\033[0;33m[Notice] clean work, nothing to commit!\033[0m"
+    exit 0 # Dừng script tại đây luôn, không chạy các lệnh dưới nữa
+fi
 read -p "type commit content: " content
 read -p "do you want to clear screen after push? enter y/n: " clear_a
-git status
 git add . 
-
 git commit -m "at $(date +'%Y-%m-%d %H:%M:%S') with content $content"
 git push
 if [[ $clear_a == "y" || $clear_a == "Y" ]]; then clear; fi
