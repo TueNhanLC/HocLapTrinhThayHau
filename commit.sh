17a8f5eb30fb1bb2499fad6b1164a99cd1774c16
TueNhanLC <tuenhanlc@gmail.com>
2026-05-13 12:04:43 +0700

    at 2026-05-13 12:04:43 with content update script shell commit

--- commit.sh
+++ commit.sh
@@ -10,5 +10,5 @@ read -p "do you want to clear screen after push? enter y/n: " clear_a
 git add . 
 git commit -m "at $(date +'%Y-%m-%d %H:%M:%S') with content $content"
 git push
-if [[ $clear_a == "y" || $clear_a == "Y" ]]; then clear; fi
+if [[ $clear_a == "y" || $clear_a == "Y" ]]; then clear; echo cleared screen, for more information, please visit summary.out; fi
 
